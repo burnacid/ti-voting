b2a90979f3c07c534414e20e2e3ec62d9c1018f8
Stefan Lenders <stefan@lenders-it.nl>
2026-03-01 10:19:21 +0100

    Cleanup games schedule

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// Schedule cleanup of games older than 7 days to run daily at 2 AM
+Schedule::command('games:cleanup')->dailyAt('02:00')->name('cleanup-old-games')->withoutOverlapping();
+
+
